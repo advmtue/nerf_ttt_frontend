03ebf3975e753346b8844a253e2e540bd5cbfbed
Adam Tuechler <adamtuechler@gmail.com>
2020-08-30 14:47:22 +1000

    Change prod build to use WSS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.ttt.adamtue.ch',
-  websocketUrl: 'ws://api.ttt.adamtue.ch/socket'
+  websocketUrl: 'wss://api.ttt.adamtue.ch/socket'
 };
