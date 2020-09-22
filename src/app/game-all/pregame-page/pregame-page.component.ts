b9445697d5be8fd0c48897101ca0b6db8cf02ed1
adamtuechler <adamtuechler@gmail.com>
2020-09-22 10:03:08 +1000

    Add styling to pregame component

--- src/app/game-all/pregame-page/pregame-page.component.ts
+++ src/app/game-all/pregame-page/pregame-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { UserService } from 'src/app/service/user.service';
 import GameInfo from 'src/types/GameInfo';
 import GameMetadata from 'src/types/GameMetadata';
 
@@ -11,9 +12,27 @@ export class PregamePageComponent implements OnInit {
   @Input() gameMetadata: GameMetadata;
   @Input() gameInfo: GameInfo;
 
-  constructor() { }
+  public showInfo = false;
+
+  constructor(private _userService: UserService) { }
 
   ngOnInit(): void {
   }
 
+  hideInfo() {
+    this.showInfo = false;
+  }
+
+  toggleInfo() {
+    this.showInfo = true;
+    setTimeout(this.hideInfo.bind(this), 5000);
+  }
+
+  startGame() {
+    console.log('Game start or something idk lmwo ');
+  }
+
+  public localPlayerIsGamemaster(): boolean {
+    return this.gameMetadata.ownerId === this._userService.id;
+  }
 }
