52fa9af19352c1eb66826f6df4b859288322c122
adamtuechler <adamtuechler@gmail.com>
2020-09-22 14:17:19 +1000

    Update production socket URI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.ttt.adamtue.ch',
-  websocketUrl: 'wss://api.ttt.adamtue.ch/socket',
+  websocketUrl: 'wss://socket.ttt.adamtue.ch',
   discordOAuthUrl: "https://discord.com/api/oauth2/authorize?client_id=754956360424226882&redirect_uri=https%3A%2F%2Fttt.adamtue.ch%2Fauth%3Fref%3Ddiscord&response_type=code&scope=identify"
 };
